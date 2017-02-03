cfbe5704f901b38d7f1671cc6939d5639144d3ac
pelsaara <saara.peltomaki@gmail.com>
2017-02-03 13:09:52 +0200

    Kayttoliittyman ensimmainen versio

--- Budjettilaskuri/src/main/java/bl/budjettilaskuri/gui/Kayttoliittyma.java
+++ Budjettilaskuri/src/main/java/bl/budjettilaskuri/gui/Kayttoliittyma.java
@@ -1,8 +1,15 @@
 package bl.budjettilaskuri.gui;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
 public class Kayttoliittyma implements Runnable {
@@ -25,7 +32,28 @@ public class Kayttoliittyma implements Runnable {
         frame.setVisible(true);
     }
 
-    private void luoKomponentit(Container container) {
+    private void luoKomponentit(Container container) {       
+        container.add(luoTekstiKentat());
+        container.add(luoValikko(), BorderLayout.SOUTH);
+
+        
+    }
+
+    private JPanel luoTekstiKentat() {
+        JPanel panel = new JPanel(new GridLayout(3, 2));
+        panel.add(new JLabel("Selite: "));
+        panel.add(new JTextField());
+        panel.add(new JLabel("Summa: "));
+        panel.add(new JTextField());
+        panel.add(new JButton("Lisää tulo"));
+        panel.add(new JButton("Lisää meno"));
+        return panel;
+    }
+
+    private JPanel luoValikko() {
+        JPanel panel = new JPanel(new GridLayout(1, 3));
+        panel.add(new JButton("Laske budjetti"));
+        return panel;
     }
 
     public JFrame getFrame() {
